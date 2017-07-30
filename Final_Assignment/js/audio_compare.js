407ac503404d8c357e41b11b9fd5e058f7788eac
AdamFedor <AdamFedor@users.noreply.github.com>
2017-07-29 22:24:45 -0700

    template a uploaded

--- Final_Assignment/js/audio_compare.js
+++ Final_Assignment/js/audio_compare.js
@@ -1,1 +1,53 @@
+// function navigation bar collapsing
+$(function(){
+  $('#lookingDownOnYou').data('size','big');
+});
+// navigation bar collapsing details
+$(window).scroll(function(){ /*working draft, looking for alternative for .scroll()*/
+  if($(document).scrollTop() > 0) /*working draft, looking for alternative for .scrollTop()*/
+{
+  if($('#lookingDownOnYou').data('size') == 'big')
+    {
+    $('#lookingDownOnYou').data('size','small');
+    $('#lookingDownOnYou').stop().animate({ /*experimental, looking for alternative for .animate()*/
+      'padding-top' : 0,
+      height:'90px'
+    },300);
+  }
+}
+else
+  {
+    if($('#lookingDownOnYou').data('size') == 'small')
+      {
+      $('#lookingDownOnYou').data('size','big');
+      $('#lookingDownOnYou').stop().animate({ /*experimental, looking for alternative for .animate()*/
+        'padding-top' : '25px',
+        height:'141.219px'
+      },100);
+    }
+  }
+});
 
+// run function for selection template
+$(document).ready(function(){
+  selectionHigh();
+  selectionMed();
+  selectionLow();
+});
+
+// function for selection template loaded
+function selectionHigh(){
+  var selectionTemplateVar = $("#selectionTop").text();
+  var $selectionTopText = $(selectionTemplateVar);
+  $(".replaceThisTop").append($selectionTopText);
+}
+function selectionMed(){
+  var selectionTemplateVar = $("#selectionButtons").text();
+  var $selectionTopText = $(selectionTemplateVar);
+  $(".replaceThisMiddle").append($selectionTopText);
+}
+function selectionLow(){
+  var selectionTemplateVar = $("#selectionBottom").text();
+  var $selectionTopText = $(selectionTemplateVar);
+  $(".replaceThisBottom").append($selectionTopText);
+}
---
+++ Final_Assignment/js/temp.js
@@ -0,0 +1,17 @@
+
+// add muted button
+<input id="selectionButtonsReoccuring" type="button" class="btn grayButton muteButton" value="Remove Additional Comparison Option">
+</input>
+
+// template for buttons
+</div>
+<div id="selectionButtons" class="container clearfix">
+  <div class="example">
+    <input type="button" class="btn leftFloat blueButton" value="Create Custom Amp">
+    </input>
+    <input type="button" class="btn indecisiveFloat blueButton" value="Create Custom Speaker">
+    </input>
+  </div>
+  <input type="button" class="btn floatRight orangeButton" value="Evaluate Selection">
+  </input>
+</div>
