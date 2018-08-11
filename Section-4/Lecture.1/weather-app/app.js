871fcf56f1ac193b0b169fcefc6e919ba6f89191
Adam Fedor <adam.fedor@concur.com>
2018-08-10 18:22:35 -0700

    Starting weather app tutorial

--- Section-4/Lecture.1/weather-app/app.js
+++ Section-4/Lecture.1/weather-app/app.js
@@ -5,5 +5,9 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
   json: true
   }, (error, response, body) => {
-    console.log(body); // just print the body from the 3x arguments
+    // console.log(JSON.stringify(body, undefined, 2)); // just print the body from the 3x arguments
+    // since there is only 1 result, this looks at the first and at formatted address - per looking at it with JSON View
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
