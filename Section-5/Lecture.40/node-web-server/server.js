9bf59f80615b6e776e8c0969f37e496e2d65467c
Adam Fedor <adam.fedor@concur.com>
2018-08-21 13:41:40 -0700

    Setup start script and heroku port

--- Section-5/Lecture.40/node-web-server/server.js
+++ Section-5/Lecture.40/node-web-server/server.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express(); // to create an app call the method
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs'); // name value pair that's passed into .use()
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log.');
+    } // checks
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// }); // example of site maintenance
+
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
@@ -44,4 +63,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000); //listening port
+app.listen(port, () => {
+    console.log(`Server is up on the port ${port}`);
+}); //listening port
