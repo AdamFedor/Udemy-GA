760025c6a6e09fe57ac7fafb8d373da569d8b684
Adam Fedor <adam.fedor@concur.com>
2018-08-23 21:17:19 -0700

    more testing

--- Section-6/Lecture.50/node-tests/utils/utils.js
+++ Section-6/Lecture.50/node-tests/utils/utils.js
@@ -1,3 +1,11 @@
 module.exports.add = (a, b) => a + b;
 
-module.exports.square = (x) => x * x + 1; // intentional error
+// module.exports.square = (x) => x * x + 1; // intentional error
+module.exports.square = (x) => x * x; // switch for no error
+
+module.exports.setName = (user, fullName) => {
+  var names = fullName.split(' '); // names array
+  user.firstName = names[0];
+  user.lastName = names[1];
+  return user;
+};
--- Section-6/Lecture.50/node-tests/utils/utils.test.js
+++ Section-6/Lecture.50/node-tests/utils/utils.test.js
@@ -1,15 +1,39 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it ('should add two numbers', () => {
   var res = utils.add(33,11); // uses the .add in the other file
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}`);
-  };
+
+  expect(res).toBe(44).toBeA('number');
+  // if (res !== 44) {
+  //   throw new Error(`Expected 44, but got ${res}`);
+  // };
 });
 
 it ('should square a number', () => {
   var res = utils.square(5);
-  if (res !== 25) {
-    throw new Error(`Expected 25, but got ${res}`);
-  };
+  expect(res).toBe(25).toBeA('number');
+  // if (res !== 25) {
+  //   throw new Error(`Expected 25, but got ${res}`);
+  // };
+});
+
+it('should set first name and last names are proper and seperate', () => {
+  var user = {location: 'Seattle', age: 34}
+  var res = utils.setName(user,'Adam Fedor');
+  // expect(user).toEqual(res);
+  expect(res).toInclude({firstName: 'Adam', lastName: 'Fedor'});
+});
+
+it('should expect some values', () => {
+  // expect(12).toNotBe(11);
+  // expect({name: 'Andrew'}).toEqual({name: 'Andrew'});
+  // expect([2,3,4]).toInclude(5);
+  expect({
+    name: 'Adam',
+    age: 34,
+    location: 'Seattle'
+  }).toInclude({
+    age: 34
+  });
 });
