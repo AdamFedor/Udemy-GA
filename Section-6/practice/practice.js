3db68e29a8c0485f0b2cef97245421d6a24be460
Adam Fedor <adam.fedor@concur.com>
2018-09-21 09:47:37 -0700

    Adding array to store data

--- Section-6/practice/practice.js
+++ Section-6/practice/practice.js
@@ -1,14 +1,16 @@
 const yargs = require('yargs');
 const _ = require('lodash');
+const weather = require('./weather.js');
 
 //COMMAND INPUTS
+//ADD DATE FOR HISTORY PULLS OF INFORMATION PREVIOUSLY COLLECTED
 const dateOptions = {
-    describe: 'Day Temperature: today, tomorrow, yesterday',
+    describe: 'Day Temperature: today, tomorrow, week',
     demand: true,
     alias: 'd'
 };
 const weatherType = {
-    describe: 'Weather Status: all, highs, lows, warnings',
+    describe: 'Weather Status: all, temperature, details, message, alert',
     demand: true,
     alias: 'w'
 };
@@ -26,11 +28,32 @@ var command = argv._[0];
 
 //PROCESSING COMMAND
 if (command === 'day'){
+    //PROCESS CALL
+    //READ FROM FILE
     console.log('--------------');
-    console.log('Weather for a given day')
+    console.log('Weather for a given day');
     console.log('Day: ', argv.date);
+    console.log('Type: ', argv.type);
+    console.log('--------------');
+} else if (command === 'forecast') {
+    //PROCESS CALL
+    //READ FROM FILE
+    console.log('--------------');
+    console.log('Weather forecast');
+    console.log('Type: ', argv.type);
+    console.log('--------------');
+} else if (command === 'list') {
+    //READ ALL FROM FILE - DATES ONLY
+} else if (command === 'history') {
+    //READ FROM FILE
+    console.log('--------------');
+    console.log('Weather was');
     console.log('Type: ', argv.type);
     console.log('--------------');
 } else {
     console.log('Not a valid command');
 };
+
+//STORE WEATHER TO DATE - CACHE
+//PULL WEATHER FROM STORED FILE - MINIMIZES CALLS
+//READ OUT WEATHER TO USER ON PAGE
---
+++ Section-6/practice/weather.js
@@ -0,0 +1,63 @@
+const axios = require('axios');
+
+//DOTENV
+require('dotenv').config();
+const keyGeo = process.env.WEATHERKEY;
+const keyDark = process.env.DARKSKY;
+
+//WEATHER CALL
+var weatherCall = (addressRequested) => {
+    var encodedAddress = encodeURIComponent(addressRequested);
+    var geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${keyGeo}`
+    //GEOCODE
+    axios.get(geocodeURL).then((response) => {
+        if (response.data.status === 'ZERO RESULTS') {
+            throw new Error('Unable to find address');
+        };
+        var lat = response.data.results[0].geometry.location.lat;
+        var lng = response.data.results[0].geometry.location.lng;
+        //DARKSKY
+        var weatherURL = `https://api.darksky.net/forecast/${keyDark}/${lat},${lng}`;
+        return axios.get(weatherURL);
+    }).then((response) => {
+        var darkSkyObject = new Object();
+        darkSkyObject.summary = [];
+        darkSkyObject.temperature = [];
+        darkSkyObject.apparentTemperature = [];
+        darkSkyObject.temperatureHigh = [];
+        darkSkyObject.temperatureLow = [];
+        darkSkyObject.humidity = [];
+        darkSkyObject.windSpeed = [];
+        darkSkyObject.windGust = [];
+        darkSkyObject.summary.push(response.data.currently.summary);
+        darkSkyObject.temperature.push(response.data.currently.temperature);
+        darkSkyObject.apparentTemperature.push(response.data.currently.apparentTemperature);
+        darkSkyObject.humidity.push(response.data.currently.humidity);
+        darkSkyObject.windSpeed.push(response.data.currently.windSpeed);
+        darkSkyObject.windGust.push(response.data.currently.windGust);
+        //LOOPED WEEK
+        for (i = 0; i < 6; i++){
+            darkSkyObject.summary.push(response.data.daily.data[i].summary);
+            darkSkyObject.temperatureHigh.push(response.data.daily.data[i].temperatureHigh);
+            darkSkyObject.temperatureLow.push(response.data.daily.data[i].temperatureLow);
+            darkSkyObject.humidity.push(response.data.daily.data[i].humidity);
+            darkSkyObject.windSpeed.push(response.data.daily.data[i].windSpeed);
+            darkSkyObject.windGust.push(response.data.daily.data[i].windGust);
+        };
+        //STORE RESPONSES
+        console.log(darkSkyObject);
+    }).catch((e) => {
+    //ERROR HANDLING
+        if(e.code === 'ENOTFOUND') {
+            console.log('Unable to connect to API servers')
+        } else {
+            console.log('ERROR: ', e.message);
+        };
+    });
+};
+
+weatherCall('98272');
+
+module.exports = {
+    weatherCall
+};
