c6fe1158f79b8a8d26cd94c0e9b35971bf6c983a
Adam Fedor <blindmouse@Adams-MacBook-Air.local>
2019-03-16 14:39:27 -0700

    Completed Webpack learning

--- Infrastructure/Webpack/webpack.config.js
+++ Infrastructure/Webpack/webpack.config.js
@@ -1,18 +1,25 @@
 const path = require('path')
 const cleanWebpackPlugin = require('clean-webpack-plugin')
 const htmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
-        // contentBase: './dist',
-        contentBase: path.join(__dirname,'dist'),
-        compress: true,
-        port: 3030
+        contentBase: './dist',
+        // contentBase: path.join(__dirname,'dist'),
+        // compress: true,
+        port: 8080
+        // hot: true
     },
     entry: {
         myEntry: './src/index.js'
+        // For code split - using the below files instead
+        // first: './js/first.js',
+        // second: './js/second.js',
+        // third: './js/third.js',
+        // css: './css/main.css'
     },
     output: {
         filename: '[name].bundle.[hash].js',
@@ -20,9 +27,16 @@ module.exports = {
     },
     module: {
         rules: [
+            // {
+            // // Disabled for mini-css-extract-plugin
+            //     test: /\.css$/,
+            //     use: ['style-loader', 'css-loader']
+            // },
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                test:/\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader'
+                ]
             },
             {
                 test: /\.(png|jpg|jpeg|svg|gif)$/,
@@ -36,6 +50,14 @@ module.exports = {
         ]
     },
     plugins: [
+        // new webpack.ProvidePlugin({
+        //     // where globals may be put via shimming
+        //     $:'jquery' // declaring jQuery as the dollar sign
+        // })
+        // new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "css/mycss.[name].css"
+        }),
         new cleanWebpackPlugin(),
         new htmlWebpackPlugin({
             title: 'html webpack plugin',
