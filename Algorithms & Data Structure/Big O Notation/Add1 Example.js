2e6a81dd30aa86493b92f8f85cd74e33f9f8e2e4
Adam Fedor <adam.fedor@concur.com>
2019-03-26 16:07:12 -0700

    Big O Notation learning

---
+++ Algorithms & Data Structure/Big O Notation/Add1 Example.js
@@ -0,0 +1,27 @@
+function addUpTo(n) {
+    let total = 0;
+    for (let i = 1; i <= n; i++) {
+        total += i;
+    }
+    return total;
+}
+
+let t1 = performance.now();
+addUpTo(1000000000);
+let t2 = performance.now();
+console.log(`Time Elapsed for the first: ${(t2 - t1) / 1000 } seconds.`)
+// console.log(addUpTo(6));
+// results in about 1.24 seconds
+
+function addUpTo2(n) {
+    return n * (n+1)/2;
+}
+
+let t3 = performance.now();
+addUpTo(1000000000);
+let t4 = performance.now();
+console.log(`Time Elapsed for the second: ${(t3 - t4) / 1000 } seconds.`)
+// console.log(addUpTo2(6))
+// results in about 0.0001 seconds
+
+// Both work the same but the second ends up significantly shorter.
