fcf58bcb9bc0ada0e2195ec8bf08db2474b7b971
Adam Fedor <adam.fedor@concur.com>
2019-04-01 15:56:05 -0700

    messing with Multi Pointers

--- Algorithms & Data Structure/Problem Solving Patterns/Optional Challenges/Multi Pointers isSubsequence.js
+++ Algorithms & Data Structure/Problem Solving Patterns/Optional Challenges/Multi Pointers isSubsequence.js
@@ -9,8 +9,29 @@
 // isSubsequence('abc', 'abracadabra'); // true
 // isSubsequence('abc', 'cba') // false
 
-function isSubsequence () {
+// Must meet the following complexities: Time O(N + M), Space O(1)
+
+function isSubsequence (str1,str2) {
     // modify the string received and add to it when it matches
     // each char in order. That way once it runs through all, it's true
     // otherwise false.
+    let minChar = 1;
+    console.log(str2.length)
 }
+isSubsequence('abc','')
+
+ // similar
+function maxSubarraySum (arr, num) {
+    let maxSum = 0;
+    let tempSum = 0;
+    if (arr.length < num) return null;
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i]; // sums the first group
+    }
+    tempSum = maxSum;
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+}
+maxSubarraySum([1,2,3,4],2)
