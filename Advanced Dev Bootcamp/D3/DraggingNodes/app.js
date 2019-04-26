59d257a7fc6a0d229455695c87e7661252f80677
Adam Fedor <blindmouse@Adams-MacBook-Air.local>
2019-04-25 22:10:54 -0700

    more learning

--- Advanced Dev Bootcamp/D3/DraggingNodes/app.js
+++ Advanced Dev Bootcamp/D3/DraggingNodes/app.js
@@ -24,13 +24,13 @@ var svg = d3.select("svg")
               .attr("width", width)
               .attr("height", height);
 
-var linkSelection = svg
-  .selectAll('line')
-  .data(links)
-  .enter()
-  .append('line')
-    .attr('stroke', 'black')
-    .attr('stroke-widgth', 1);
+var linkSelection = svg 
+                      .selectAll("line")
+                      .data(links)
+                      .enter()
+                      .append("line")
+                        .attr("stroke", "black")
+                        .attr("stroke-width", 1);
 
 var nodeSelection = svg
                       .selectAll("circle")
@@ -45,18 +45,19 @@ var simulation = d3.forceSimulation(nodes);
 simulation
   .force("center", d3.forceCenter(width / 2, height / 2))
   .force("nodes", d3.forceManyBody())
-  .force('links', d3.forceLinks(links)
-    .id(d => d.color)
-    .distance(d => 5 * (d.source.size + d.target.size)))
+  .force("links", d3.forceLink(links)
+                    .id(d => d.color)
+                    .distance(d => 5 * ( d.source.size + d.target.size )))
   .on('tick', ticked);
          
 function ticked() {
   nodeSelection
     .attr("cx", d => d.x )
     .attr("cy", d => d.y );
+
   linkSelection
-    .attr('x1', d => d.source.x)
-    .attr('y1', d => d.source.y)
-    .attr('x2', d => d.source.x)
-    .attr('y2', d => d.source.y)
+    .attr("x1", d => d.source.x)
+    .attr("y1", d => d.source.y)
+    .attr("x2", d => d.target.x)
+    .attr("y2", d => d.target.y)
 }
