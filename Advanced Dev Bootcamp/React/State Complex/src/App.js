e84d4f62251d7ad082dcbdcef6893d29ce915a7a
Adam Fedor <blindmouse@Adams-MacBook-Air.local>
2019-05-11 11:53:19 -0700

    refactoring for sharing state

--- Advanced Dev Bootcamp/React/State Complex/src/App.js
+++ Advanced Dev Bootcamp/React/State Complex/src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
+class InstructorItem extends Component {
+  static propTypes = {
+    name: PropTypes.string,
+    hobbies: PropTypes.arrayOf(PropTypes.string)
+  }
+  render() {
+    return (
+      <li>
+        <h3>{this.props.name}</h3>
+        <h4>Hobbies: {this.props.hobbies.join(", ")}</h4>
+      </li>
+    )
+  }
+}
 class App extends Component {
   constructor(props) {
     super(props);
@@ -21,13 +36,44 @@ class App extends Component {
         }
       ]
     };
+    setTimeout((obj) => {
+      const randomInst = Math.floor(
+        Math.random() *
+        this.state.instructors.length
+      );
+      const hobbyIndex = Math.floor(
+        Math.random() *
+        this.state.instructors[randomInst].length
+      )
+      const instructors = this.state.instructors.map((inst, i) => {
+        if (i === randomInst) {
+          const hobbies = inst.hobbies.slice();
+          hobbies.splice(hobbyIndex, 1);
+          return {
+            ...inst,
+            hobbies
+          }
+        }  
+        return inst;
+      });
+      // ---- ALTERNATIVE 1 ---- //
+      // const instructors = this.state.instructors.map((inst, i) => (
+      //   i === randomInst ? {
+      //     ...inst,
+      //     hobbies: [...inst.hobbies.slice(0, hobbyIndex).concat(inst.hobbies).slice(hobbyIndex+1, inst.hobbies.length)]
+      //   } : inst
+      // ));
+      // ---- ALTERNATIVE 2---- //
+      // const instructors = this.state.instructors.slice();
+      // instructors[randomInst] = Object.assign({}, instructors[randomInst])
+      // instructors[randomInst].hobbies = instructors[randomInst].hobbies.slice();
+      // instructors[randomInst].hobbies.splice(hobbyIndex,1);
+      this.setState({instructors}) // short hand for instructors: instructors
+    }, 5000);
   }
   render() {
     const instructors = this.state.instructors.map((instructor, index) => (
-      <li key={index}>
-        <h3>{instructor.name}</h3>
-        <h4>Hobbies: {instructor.hobbies.join(", ")}</h4>
-      </li>
+      <InstructorItem key={index} name={instructor.name} hobbies={instructor.hobbies} />
     ));
     return (
       <div className="App">
