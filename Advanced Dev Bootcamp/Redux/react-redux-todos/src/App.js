14babd8ef20b351e1f3b3abd7cf26ebd229b42ee
Adam Fedor <adam.fedor@concur.com>
2019-05-21 16:01:00 -0700

    adding react-router-dom

--- Advanced Dev Bootcamp/Redux/react-redux-todos/src/App.js
+++ Advanced Dev Bootcamp/Redux/react-redux-todos/src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoList from './TodoList';
+import { Link, Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1> See our To-do's!</h1>
+      <p>
+        <Link to='/todos'>See my todos!</Link>
+      </p>
+      <p>
+        <Link to='/todos/new'>Add a new todos!</Link>
+      </p>
+      <Route path='/todos' component={TodoList} />
+      <Route path='/' render={() => <Redirect to='/todos' />} />
       <TodoList />
     </div>
   );
--- Advanced Dev Bootcamp/Redux/react-redux-todos/src/TodoList.js
+++ Advanced Dev Bootcamp/Redux/react-redux-todos/src/TodoList.js
@@ -2,51 +2,36 @@ import React, { Component } from 'react';
 import Todo from './Todo';
 import { connect } from 'react-redux';
 import { AddTodo, RemoveTodo } from './actionCreators';
+import { Route } from 'react-router-dom';
+import NewTodoForm from './NewTodoForm';
 
 class TodoList extends Component {
     constructor (props) {
         super(props);
         // where you do method binding, must be here
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.removeTodo = this.removeTodo.bind(this);
-        this.state = {
-            // introduce react state
-            task: ""
-        };
+        this.handleAdd = this.handleAdd.bind(this);
     }
-    handleSubmit(e) {
-        e.preventDefault();
-        this.props.AddTodo(this.state.task)
-        e.target.reset();
-    }
-    handleChange(e) {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
+    handleAdd(val) {
+        this.props.addTodo(val);
     }
     removeTodo(id) {
         this.props.RemoveTodo(id)
     }
     render () {
         let todos = this.props.todos.map((val, index) => (
-            <Todo removeTodo={this.removeTodo.bind(this, val.id)} task={val.task} key={index} />
+            <Todo
+                removeTodo={this.removeTodo.bind(this, val.id)}
+                task={val.task}
+                key={index} />
         ));
         return (
             <div>
-                <form onSubmit = {this.handleSubmit}>
-                    <label htmlFor="task">Task</label>
-                    <input
-                        type="text"
-                        name="task"
-                        id="task"
-                        onChange={this.handleChange}
-                    />
-                    <button>Add a Todo!</button>
-                </form>
-                <ul>{todos}</ul>
+                <Route path='/todos/new' component={props => (
+                    <NewTodoForm {...props} handleSubmit={this.handleAdd} />
+                )} />
+            <Route exact path='/todos' component={() => <div>{todos}</div>} />
             </div>
-        )
+        );
     }
 }
 
