ba44d5119b15b41fab10500948580689fd547f10
Adam Fedor <blindmouse@Adams-MacBook-Air.local>
2019-07-10 09:11:03 -0700

    Last night's ideas on picking two files (lists), merging if duplicate in alphabetical order

---
+++ Z - Self Focus/Merge Files/index.js
@@ -0,0 +1,20 @@
+// Pseudocode
+// ----------
+// 1. Two files are identified and opened to compare.
+// 2. The outcome of the merge when the two files both have the same list item.
+// 3. The new file will be the same format as the imported files alphabetically A-Z.
+// 4. Big O of the output file will be O(n2) or lower. Efficiency and longevity are priority.
+
+// Sort Pseudocode
+// ---------------
+// IDEA: Start with array.sort(), then push to more efficient sorting algorithm - recursive and maybe radix? Con is limited to arrays without converting.
+// 1. 
+
+// Merge and Export Pseudocode
+// ---------------------------
+// IDEA: 
+// 1. 
+
+// IDEA: push one to an object, if it exists the push to the new object for export
+// then sort it there when exporting one at a time.
+// So loop the check of one against the other, then merge each time.
