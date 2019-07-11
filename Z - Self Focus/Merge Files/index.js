c1449a61914e0779bffc52609adf3d8ada5e3463
Adam Fedor <blindmouse@Adams-MacBook-Air.local>
2019-07-11 00:03:56 -0700

    need to add asynchronous handling of imports - then export the data. Currently loads to an object successfully with counts.

--- Z - Self Focus/Merge Files/index.js
+++ Z - Self Focus/Merge Files/index.js
@@ -1,16 +1,36 @@
-// Pseudocode
-// ----------
-// 1. Two text files are identified and opened to compare.
-// 2. The outcome of the merge the top 10 words that occur in both files.
-// 3. The new file will be the same format as the imported files sorted by number of occurances.
-// Changed the pseudocode (not alphabetical, export of all matches)
+// Write a program that will read 2 text files and determine top 10 words that occur
+// in both files sorted by number of total occurences. Number of occurances should
+// be printed as well.
 
-// Sort Pseudocode
-// ---------------
-// IDEA: Start with array.sort(), then push to more efficient sorting algorithm - recursive and maybe radix? Con is limited to arrays without converting.
+const fs = require('fs');
+const async = require('async');
 
-// Merge and Export Pseudocode
-// ---------------------------
-// IDEA: Export via an object with a count - grabbing the top 10
+let list1 = './list1.txt';
+let list2 = './list2.txt';
+let obj = {};
 
-// IDEA: push one to an object, if it exists increase the count by 1, otherwise = 1
+let callback = function (err, data) {
+    if (err) {
+        return console.error(err);
+    } else {
+        let arr = data.split('\n').forEach(function (element) {
+            // loop in loop isn't as efficient as it could be straight to object
+            for (let val of element) {
+                obj[element] = (obj[element] || 0) + 1
+            }
+        });
+        console.log(obj['Sonus Faber'])
+        return obj;
+    }
+}
+
+// must run these asynchronous
+let file1 = () => {
+    let comp = fs.readFile(list1, 'utf8', callback);
+};
+
+let file2 = () => {
+    let comp = fs.readFile(list2, 'utf8', callback);
+}
+
+file1()
