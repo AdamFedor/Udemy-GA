f5ba571dc25092dcdcd7763ca1d46285386b8d59
Adam Fedor <blindmouse@Adams-MacBook-Air.local>
2019-07-10 19:09:52 -0700

    updated pseudocode with updated instructions. Was alphabetical, actually is quanity sort.

--- Z - Self Focus/Merge Files/index.js
+++ Z - Self Focus/Merge Files/index.js
@@ -1,20 +1,16 @@
 // Pseudocode
 // ----------
-// 1. Two files are identified and opened to compare.
-// 2. The outcome of the merge when the two files both have the same list item.
-// 3. The new file will be the same format as the imported files alphabetically A-Z.
-// 4. Big O of the output file will be O(n2) or lower. Efficiency and longevity are priority.
+// 1. Two text files are identified and opened to compare.
+// 2. The outcome of the merge the top 10 words that occur in both files.
+// 3. The new file will be the same format as the imported files sorted by number of occurances.
+// Changed the pseudocode (not alphabetical, export of all matches)
 
 // Sort Pseudocode
 // ---------------
 // IDEA: Start with array.sort(), then push to more efficient sorting algorithm - recursive and maybe radix? Con is limited to arrays without converting.
-// 1. 
 
 // Merge and Export Pseudocode
 // ---------------------------
-// IDEA: 
-// 1. 
+// IDEA: Export via an object with a count - grabbing the top 10
 
-// IDEA: push one to an object, if it exists the push to the new object for export
-// then sort it there when exporting one at a time.
-// So loop the check of one against the other, then merge each time.
+// IDEA: push one to an object, if it exists increase the count by 1, otherwise = 1
