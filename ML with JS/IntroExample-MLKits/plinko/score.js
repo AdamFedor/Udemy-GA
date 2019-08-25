7b53a090cb530b8b597785521d4388f1b4570653
Adam Fedor <blindmouse@Adams-MacBook-Air.local>
2019-08-25 10:01:06 -0700

    Updating code to handle prediction based on 300 pixels

--- ML with JS/IntroExample-MLKits/plinko/score.js
+++ ML with JS/IntroExample-MLKits/plinko/score.js
@@ -1,8 +1,27 @@
+const outputs = []; // store each observation
+const predictionPoint = 300;
+const k = 3;
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+  const bucket = _.chain(outputs)
+    .map(row => [distance(row[0]), row[3]])
+    .sort(row => row[0])
+    .slice(0, k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy()
+    .last()
+    .first()
+    .parseInt()
+    .value()
+  
+  console.log('Most likely it will fall into', bucket);
 }
 
+function distance(point) {
+  return Math.abs(point - predictionPoint);
+}
