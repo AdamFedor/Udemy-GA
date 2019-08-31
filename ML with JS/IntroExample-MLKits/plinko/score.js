e614351ff0c6d961b8f5f71d1f94e1e6972d9e4e
Adam Fedor <blindmouse@Adams-MacBook-Air.local>
2019-08-31 13:57:24 -0700

    using lodash on analysis

--- ML with JS/IntroExample-MLKits/plinko/score.js
+++ ML with JS/IntroExample-MLKits/plinko/score.js
@@ -1,5 +1,4 @@
 const outputs = []; // store each observation
-const predictionPoint = 300;
 const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
@@ -7,8 +6,28 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  const bucket = _.chain(outputs)
-    .map(row => [distance(row[0]), row[3]])
+  const testSetSize = 10;
+  const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
+  let numberCorrect = 0;
+  // for (let i = 0; i < testSet.length; i++) {
+  //   const bucket = knn(trainingSet, testSet[i][0]);
+  //   if (bucket === testSet[i][3]) {
+  //     numberCorrect++;
+  //   }
+  // }
+  // console.log('Accuracy:',numberCorrect / testSetSize)
+  // using lodash instead
+  const accuracy = _.chain(testSet)
+    .filter(testPoint => knn(trainingSet, testPoint[0]) === testPoint[3]) //testPoint is what is tested being passed in, same as above for loop
+    .size()
+    .divide(testSetSize) // in chain the first argument doesn't have to be passed in, it's automatic
+    .value()
+  console.log('Accuracy is:', accuracy)
+}
+
+function knn(data, point) {
+  return _.chain(data) // passing in data for any use
+    .map(row => [distance(row[0], point), row[3]]) // passing in a data point to compare. Point is a second argument
     .sort(row => row[0])
     .slice(0, k)
     .countBy(row => row[1])
@@ -18,10 +37,15 @@ function runAnalysis() {
     .first()
     .parseInt()
     .value()
-  
-  console.log('Most likely it will fall into', bucket);
 }
 
-function distance(point) {
-  return Math.abs(point - predictionPoint);
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB);
+}
+
+function splitDataset(data, testCount) {
+  const shuffled = _.shuffle(data);
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+  return [testSet, trainingSet];
 }
