affb9b0a7d4690da8cf069789bb08b5329e486c2
Adam Fedor <blindmouse@Adams-MacBook-Air.local>
2019-09-01 10:23:22 -0700

    looking at bounciness and drop position

--- ML with JS/IntroExample-MLKits/plinko/score.js
+++ ML with JS/IntroExample-MLKits/plinko/score.js
@@ -6,7 +6,7 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 
 function runAnalysis() {
   const testSetSize = 50;
-  const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
+  const [testSet, trainingSet] = splitDataset(minMax(outputs, 3), testSetSize);
   let numberCorrect = 0;
   // for (let i = 0; i < testSet.length; i++) {
   //   const bucket = knn(trainingSet, testSet[i][0]);
@@ -14,20 +14,25 @@ function runAnalysis() {
   //     numberCorrect++;
   //   }
   // } // replace with lodash below
-  _.range(1,15).forEach(k => {
+  _.range(1, 20).forEach(k => {
     const accuracy = _.chain(testSet)
-      .filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3]) //testPoint is what is tested being passed in, same as above for loop
+      .filter(testPoint => knn(trainingSet, _.initial(testPoint), k) === testPoint[3]) //testPoint is what is tested being passed in, same as above for loop
       .size()
       .divide(testSetSize) // in chain the first argument doesn't have to be passed in, it's automatic
       .value()
     console.log('For k of', k, 'Accuracy is:', accuracy)
   });
-  console.log(testSet)
+  // console.log(testSet)
 }
 
 function knn(data, point, k) {
-  return _.chain(data) // passing in data for any use
-    .map(row => [distance(row[0], point), row[3]]) // passing in a data point to compare. Point is a second argument
+  return _.chain(data)
+    .map(row => {
+      return [
+        distance(_.initial(row), point), // assume point has 3 values - removing bucketLabel via runAnalysis()
+        _.last(row)
+      ]
+    })
     .sort(row => row[0])
     .slice(0, k)
     .countBy(row => row[1])
@@ -48,4 +53,18 @@ function splitDataset(data, testCount) {
   const testSet = _.slice(shuffled, 0, testCount);
   const trainingSet = _.slice(shuffled, testCount);
   return [testSet, trainingSet];
+}
+
+function minMax (data, featureCount) {
+  const clonedData = _.cloneDeep(data)
+  for (let i = 0; i < featureCount; i++) {
+    // iterate over each collumn (feature) of the array data if looking at it vertically
+    const column = clonedData.map(row => row[i]); // an array of numbers, extracting
+    const min = _.min(column);
+    const max = _.max(column);
+    for (let j = 0; j < clonedData.length; j++) {
+      clonedData[j][i] = (clonedData[j][i] - min) / (max - min)// j is the row, i is the column
+    }
+  }
+  return clonedData;
 }
